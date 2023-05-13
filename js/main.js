7c26a9a548310cb6011b2df5ff3fa417fa86e2f0
Adam Muhtarif <muhtarif.dev.web@gmail.com>
2023-05-13 13:05:22 +0200

    first upload

---
+++ js/main.js
@@ -0,0 +1,77 @@
+// Active Link
+const links = document.querySelectorAll(".links li a");
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    links.forEach((link) => {
+      link.classList.remove("active");
+    });
+    link.classList.add("active");
+  });
+});
+
+// Navbar Background
+window.addEventListener("scroll", () => {
+  if (scrollY >= 125) {
+    document.querySelector("header").classList.add("addBackground");
+  } else {
+    document.querySelector("header").classList.remove("addBackground");
+  }
+
+  // Change Active Link On Scroll
+
+  if (window.scrollY < 793.75) {
+    links.forEach((link) => {
+      link.classList.remove("active");
+    });
+    document.querySelector("nav .home").classList.add("active");
+  }
+
+  if (window.scrollY >= 750) {
+    links.forEach((link) => {
+      link.classList.remove("active");
+    });
+    document.querySelector("nav .about").classList.add("active");
+  }
+
+  if (window.scrollY >= 1525) {
+    links.forEach((link) => {
+      link.classList.remove("active");
+    });
+    document.querySelector("nav .resume").classList.add("active");
+  }
+
+  if (window.scrollY >= 2825) {
+    links.forEach((link) => {
+      link.classList.remove("active");
+    });
+    document.querySelector("nav .services").classList.add("active");
+  }
+
+  if (window.scrollY >= 3723.75) {
+    links.forEach((link) => {
+      link.classList.remove("active");
+    });
+    document.querySelector("nav .skills").classList.add("active");
+  }
+
+  if (window.scrollY >= 4350) {
+    links.forEach((link) => {
+      link.classList.remove("active");
+    });
+    document.querySelector("nav .projects").classList.add("active");
+  }
+
+  if (window.scrollY >= 5698.75) {
+    links.forEach((link) => {
+      link.classList.remove("active");
+    });
+    document.querySelector("nav .blog").classList.add("active");
+  }
+
+  if (window.scrollY >= 7298.75) {
+    links.forEach((link) => {
+      link.classList.remove("active");
+    });
+    document.querySelector("nav .contact").classList.add("active");
+  }
+});
